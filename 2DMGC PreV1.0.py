0ee2a71db3febec29696e11cc6d9d2121869490e
GL1TCHERS <leonard.hahn2000@gmail.com>
2026-05-21 11:36:16 +0200

    2DMGC V0.2.py
    This is the V0.2 and it add`s a lot of customizability. It works with x*y

--- 2DMGC PreV1.0.py
+++ 2DMGC PreV1.0.py
@@ -1,11 +1,28 @@
+import time
+Result = input("Input the Result for the graph here:")
+PausesFalse = input("This is how long you want to wait after a False Caluclation (0 recommended)")
+PausesTrue = input("This is how long you want to wait after a True Caluclation (1 recommended)")
+Trues = 0
+Falses = 0
+
 x = 1
 y = 1
 
 while y <= 20:
-    print(x, y)
-    x += 1
-    
-    if x >= 21:
+    if x*y == Result:
+        print("x = %s | y = %s | Result = %s (True)" %(x,y,x*y))
+        Trues += 1
+        time.sleep(PausesTrue)
+    else:
+        print("x = %s | y = %s | Result = %s (False)" %(x,y,x*y))
+        Falses += 1
+        time.sleep(PausesFalse)
+    if x >= 20:
         y += 1
         x = 1
-print("Thanks for using the Math Graph Calcuator V0.1")
+    else:
+        x += 1
+print("Original Result was:", Result)
+print("%s were True | %s were False" %(Trues,Falses))
+time.sleep(2)
+print("Thanks for using the Math Graph Calculator V0.2")
